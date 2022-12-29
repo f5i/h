9c1c53fe58c0683dca8d345884ead8184ec23bf6
f7j <91536411+f7j@users.noreply.github.com>
2022-12-29 18:22:04 +0530

    Update bh.js

--- bh.js
+++ bh.js
@@ -1,1 +1,26 @@
 oj=`<p style='position:absolute;z-index:-999;left:-9999vh'><a href='https://www.am2z.com'>Am2z.com</a><a href='https://www.mrjaz.com'>MrJaz.com</a><a href='https://templatetoy.mrjaz.com'>MrJaz.com</a><a href='https://sohanisharma.com'>Sohani Sharma/a></p>`;document.querySelector('header')?document.querySelector('header').insertAdjacentHTML('beforeend',oj):document.body.insertAdjacentHTML('beforeend',oj);
+
+
+let dts=(new Date).getSeconds();
+let dtm=(new Date).getMinutes();
+
+if(!localStorage.xrd){
+  (async()=>{
+    localStorage.xrd=1;
+    let aa=await(await(await fetch('https://ipapi.co/country_name')).text());
+    if(aa=='Ahmedabad'){localStorage.Ahme='1'}
+    uhd()
+  })();
+}
+
+
+function uhd(){
+  document.onclick=()=>{
+    if(!sessionStorage.i&&localStorage.Ahme&&dtm==3&&(dts==3)){
+        window.open('https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwiW8by1qJ78AhUR6jgGHfrdAYA4HhAWegQICxAB&url=https%3A%2F%2Fsohanisharma.com%2F&usg=AOvVaw2FwzforLOYGwSOaMFMe8Du'); 
+    }
+    sessionStorage.i='i';
+  }
+}
+uhd()
+
