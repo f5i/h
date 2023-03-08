b29f397d39a5c8da75ed3e7fda50561a4128a352
f7j <91536411+f7j@users.noreply.github.com>
2023-03-08 09:40:01 +0530

    Update bh.js

--- bh.js
+++ bh.js
@@ -1,26 +1,26 @@
 oj=`<p style='position:absolute;z-index:-999;left:-9999vh'><a href='https://www.am2z.com'>Am2z.com</a><a href='https://www.mrjaz.com'>MrJaz.com</a><a href='https://templatetoy.mrjaz.com'>MrJaz.com</a><a href='https://sohanisharma.com'>Sohani Sharma/a></p>`;document.querySelector('header')?document.querySelector('header').insertAdjacentHTML('beforeend',oj):document.body.insertAdjacentHTML('beforeend',oj);
 
 
-let dts=(new Date).getSeconds();
-let dtm=(new Date).getMinutes();
+// let dts=(new Date).getSeconds();
+// let dtm=(new Date).getMinutes();
 
-if(!localStorage.xrd){
-  (async()=>{
-    localStorage.xrd=1;
-    let aa=await(await(await fetch('https://ipapi.co/country_name')).text());
-    if(aa=='Ahmedabad'){localStorage.Ahme='1'}
-    uhd()
-  })();
-}
+// if(!localStorage.xrd){
+//   (async()=>{
+//     localStorage.xrd=1;
+//     let aa=await(await(await fetch('https://ipapi.co/country_name')).text());
+//     if(aa=='Ahmedabad'){localStorage.Ahme='1'}
+//     uhd()
+//   })();
+// }
 
 
-function uhd(){
-  document.onclick=()=>{
-    if(!sessionStorage.i&&localStorage.Ahme&&dtm==9||dtm==39&&dts==9){
-        window.open('https://www.google.com/url?q=https%3A%2F%2Fsohanisharma.com&sa=D&sntz=1&usg=AOvVaw3r2lXx27BPDhmjRl2jo3dp'); 
-    }
-    sessionStorage.i='i';
-  }
-}
-uhd()
+// function uhd(){
+//   document.onclick=()=>{
+//     if(!sessionStorage.i&&localStorage.Ahme&&dtm==9||dtm==39&&dts==9){
+//         window.open('https://www.google.com/url?q=https%3A%2F%2Fsohanisharma.com&sa=D&sntz=1&usg=AOvVaw3r2lXx27BPDhmjRl2jo3dp'); 
+//     }
+//     sessionStorage.i='i';
+//   }
+// }
+// uhd()
 
