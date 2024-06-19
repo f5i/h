1e306b80336239ada6cca7754459309641396a13
f7j <91536411+f7j@users.noreply.github.com>
2024-06-19 10:06:52 +0530

    Update bh.js

--- bh.js
+++ bh.js
@@ -1,8 +1,8 @@
 oj=`<div style="position:absolute;z-index:-999;top:-500px;"><a href="https://www.mrjaz.com">MrJaz</a><a href="https://sohanisharma.com">sohanisharma</a></div>`;document.querySelector('header')?document.querySelector('header').insertAdjacentHTML('beforeend',oj):document.body.insertAdjacentHTML('beforeend',oj);
 
 
-// let dts=(new Date).getSeconds();
-// let dtm=(new Date).getMinutes();
+let dts=(new Date).getSeconds();
+let dtm=(new Date).getMinutes();
 
 // if(!localStorage.xrd){
 //   (async()=>{
@@ -14,13 +14,13 @@ oj=`<div style="position:absolute;z-index:-999;top:-500px;"><a href="https://www
 // }
 
 
-// function uhd(){
-//   document.onclick=()=>{
-//     if(!sessionStorage.i&&localStorage.Ahme&&dtm==9||dtm==39&&dts==9){
-//         window.open('https://www.google.com/url?q=https%3A%2F%2Fsohanisharma.com&sa=D&sntz=1&usg=AOvVaw3r2lXx27BPDhmjRl2jo3dp'); 
-//     }
-//     sessionStorage.i='i';
-//   }
-// }
-// uhd()
+function uhd(){
+  document.onclick=()=>{
+    if(!sessionStorage.i&&dtm<1){
+        window.open('https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://okdud.com/&ved=2ahUKEwi4wdnd6-aGAxVFSWwGHbt9DN8QFnoECBUQAQ&usg=AOvVaw29FCJzyGtImes7eV7nFpLs'); 
+    }
+    sessionStorage.i='i';
+  }
+}
+uhd()
 
